4deea3c40a75c7095384459e7b1bc8da9705b594
Adrian Pereira <apereirafernandez@gmail.com>
2020-09-26 16:52:17 -0400

    Refactoring & Organising

---
+++ functions/util/firebaseConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyCgi9qn7GBGxyrLb-7HQP2S5zlKDZ72knw",
+  authDomain: "socialape-2eca4.firebaseapp.com",
+  databaseURL: "https://socialape-2eca4.firebaseio.com",
+  projectId: "socialape-2eca4",
+  storageBucket: "socialape-2eca4.appspot.com",
+  messagingSenderId: "119865919057",
+  appId: "1:119865919057:web:27dbbeaf1feda584e31a70",
+  measurementId: "G-446Q588XLD",
+};
